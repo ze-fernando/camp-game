a8bf05ef070a892de7956f4ae9b0e5c4dc9b09d6
ze-fernando <zefernando.dev@gmail.com>
2025-12-31 16:07:58 -0300

    feat: balls and render player

--- config/base.py
+++ config/base.py
@@ -21,3 +21,39 @@ class Base:
         rect.center = (int(cls.x * x_percent), int(cls.y * y_percent))
 
         cls.display.blit(txt, rect)
+
+    @classmethod
+    def render_ball(cls, position = (0.1, 0.2), color = (255,0,0)):
+
+        if cls.display is None:
+            raise Exception("Display ainda não foi iniciado")
+
+        x = int(cls.x * position[0])
+        y = int(cls.y * position[1])
+
+        pygame.draw.circle(
+            cls.display,
+            color,
+            (x,y),
+            10
+        )
+
+
+    @classmethod
+    def render_player(cls, player_x):
+
+        if cls.display is None:
+            raise Exception("Display ainda não foi iniciado")
+
+        player_width = 20
+        player_height = 20
+
+        x = int(cls.x * player_x) - player_width // 2
+        y = int(cls.y * 0.9)
+
+        pygame.draw.rect(
+            cls.display,
+            (0, 0, 255),
+            (x, y, player_width, player_height)
+        )
+
--- screens/game.py
+++ screens/game.py
@@ -5,7 +5,12 @@ import random
 class Game(Base):
     score = 0
     lifes = ["x", "x", "x"]
-    balls = []
+
+    ball_pos = [0.5, 0.0] 
+    ball_speed = 0.0008
+    ball_color = (255, 0, 0) 
+
+    player_x = 0.5
 
     CAMP = {
         "C": ["C", "Dm", "Em", "F", "G", "Am", "Bm7"],
@@ -16,13 +21,47 @@ class Game(Base):
         "A": ["A", "Bm", "C#m", "D", "E", "F#m", "G#m7"],
         "B": ["B", "C#m", "D#m", "E", "F#", "G#m", "A#m7"],
     }
-
-    GREEN = (0,255,0)
     RED = (255,0,0)
-    BALL_RADIUS = 10
+    GREEN = (0,255,0)
 
-    SPAWN_INTERVAL = 3000  
-    last_spawn = 0
+    @classmethod
+    def spawn_ball(cls):
+        cls.ball_pos = [random.random(), 0.0]
+        cls.ball_color = random.choice([cls.GREEN, cls.RED])
+
+    @classmethod
+    def update_ball(cls):
+        cls.ball_pos[1] += cls.ball_speed
+
+        if cls.ball_pos[1] > 1:
+            if cls.ball_color == cls.RED:
+                cls.score -= 1
+            else:
+                cls.score += 1
+
+            cls.spawn_ball()
+
+    @classmethod
+    def draw_ball(cls):
+        cls.render_ball(tuple(cls.ball_pos), cls.ball_color)
+
+    @classmethod
+    def draw_player(cls):
+        cls.render_player(cls.player_x)
+
+    @classmethod
+    def update_player(cls):
+        print(cls.player_x)
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            cls.player_x -= 0.0005 
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            cls.player_x += 0.0005
+
+        cls.player_x = max(0, min(1, cls.player_x))
 
     @classmethod
     def draw_header(cls):
@@ -34,6 +73,7 @@ class Game(Base):
 
         for i, life in enumerate(cls.lifes):
             cls.render_text(text=life, y_percent=0.05, x_percent=x_start + i * gap)
+
 
     @classmethod
     def game_over(cls):
@@ -60,12 +100,18 @@ class Game(Base):
                     running = False
                     pygame.quit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_0:
-                        cls.score +=1
-                    elif event.key == pygame.K_1:
-                        cls.lifes.pop()
+                    if event.key == pygame.K_RIGHT:
+                        cls.player_x += 1
+                    elif event.key == pygame.K_LEFT:
+                        cls.player_x -= 1
 
             cls.draw_header()
+
+            cls.update_ball()
+            cls.update_player()
+            
+            cls.draw_player()
+            cls.draw_ball()
             pygame.display.update()
 
 
